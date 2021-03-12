582b33d7f8099b3f5c472643ca09ae261013e41e
sachin <sachinbhattarai55@gmail.com>
2021-03-12 20:57:43 +0545

    env modified

--- create-env.js
+++ create-env.js
@@ -1,3 +1,6 @@
 const fs = require('fs');
 
 fs.writeFileSync('./.env', `HOST_API=${process.env.HOST_API}`);
+fs.writeFileSync('./.env', `CLOUDINARY_NAME=${process.env.CLOUDINARY_NAME}`);
+fs.writeFileSync('./.env', `CLOUDINARY_PRESET=${process.env.CLOUDINARY_PRESET}`);
+
